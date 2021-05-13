910ea662661972bd820437b2f72b365fa2b51cee
DjDeveloperr <djsandhu2005@gmail.com>
2021-05-13 15:20:31 +0530

    init

--- test_lib/target/debug/deps/test_lib.d
+++
@@ -1,5 +0,0 @@
-H:\Rust\deno_ffi\test_lib\target\debug\deps\test_lib.dll: src\lib.rs
-
-H:\Rust\deno_ffi\test_lib\target\debug\deps\test_lib.d: src\lib.rs
-
-src\lib.rs:
--- test_lib/target/debug/test_lib.d
+++
@@ -1,1 +0,0 @@
-H:\Rust\deno_ffi\test_lib\target\debug\test_lib.dll: C:\Users\dell\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.94\build.rs H:\Rust\deno_ffi\test_lib\src\lib.rs
